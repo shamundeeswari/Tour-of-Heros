06254178186b74c3910f078f98fc4f9ee8fe1b98
samundeeswari.v <samundeeswari.v@ionixxtech.com>
2020-02-14 19:22:02 +0530

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
+import {Heroes} from '../mock-heroes';
+
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+
+    hero : Hero = {
+
+	  	id: 1,
+	  	name: 'shamu' ,
+	   	
+    };
+
+
+
+    heros = Heroes ;
+
+
+
+    selectedHero : Hero;
+
+
+
+    onSelect(hero:Hero){
+
+  	  this.selectedHero = hero;
+
+    }
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
